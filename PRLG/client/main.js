a9ac8580ed9a16d4c9739b9863439a1c5254570d
Administrator <administrator@PROLOGDOM.NET>
2018-04-25 13:58:30 +0200

    tried another dropdown solution

--- PRLG/client/main.js
+++ PRLG/client/main.js
@@ -191,13 +191,17 @@ Template.patients.events({/*
 			if ( error ){ console.log( error );			}
 		});
 	}*/	
+	// add a class="menuitem" to your <li>
 
-
+    'click .menuitem': function (event) {
+        $('#mt-command-dd').text(event.target.text);
+    },
+    /*
 	'click .dropdown-toggle': function (e) {
     e.preventDefault();
     $(e.target).find('.dropdown-menu').toggle();
     },
-    
+    */
 	'submit form': function(event){
 	    event.preventDefault();
 	    var patientSurName = $('[name="patientSurName"]').val();
